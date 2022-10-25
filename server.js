13f65fc0790e433d6ef1270d225b99f5bd9f2ab4
Keyukemi <keyukemi.ku@gmail.com>
2022-10-25 20:20:38 +0100

    UPDATED DB CONNECT

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import bcrypt, { hash } from 'bcrypt-nodejs';
 import cors from 'cors';
 import knex from 'knex';
- 
+
 import handleRegister from './controllers/register.js';
 import handleSignin from './controllers/signin.js';
 import handleProfile from './controllers/profile.js';
@@ -10,18 +10,18 @@ import handleImage from './controllers/image.js';
 import handleImageUrl from './controllers/imageurl.js'
 
 
+  
 //importing knex
 const db = knex({
-    client: 'pg',
-    connection: {
-      connectionString : process.env.DATABASE_URL,
-      ssl : true
+  client: 'pg',
+  connection: {
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
     }
-  });
+  }
+});
 
-// db.select ('*').from('users').then(data =>{
-//     console.log(data);
-// });
 
 //Creating a server and exporting 
 const app = express();
@@ -31,26 +31,26 @@ app.use(express.json());
 //cors allows front end to access API
 app.use(cors())
 
-app.get('/', (req, res)=> {res.send ('It works')})
+app.get('/', (req, res) => { res.send('It works') })
 
 
 //The sign in post request
-app.post('/signin', (req, res)=> {handleSignin(req, res,db, bcrypt)})
+app.post('/signin', (req, res) => { handleSignin(req, res, db, bcrypt) })
 
 //Register Post request
-app.post('/register', (req, res)=> {handleRegister(req, res,db, bcrypt)})
+app.post('/register', (req, res) => { handleRegister(req, res, db, bcrypt) })
 
 //Get profile request
-app.get('/profile/:id',(req, res)=> {handleProfile(req, res,db)})
+app.get('/profile/:id', (req, res) => { handleProfile(req, res, db) })
 
 //add to the number of entries when image is uploaded
-app.put('/image', (req, res)=> {handleImage(req, res,db)})
+app.put('/image', (req, res) => { handleImage(req, res, db) })
 
 //
-app.post('/imageurl', (req, res)=> {handleImageUrl(req, res)})
+app.post('/imageurl', (req, res) => { handleImageUrl(req, res) })
 
 
 
 app.listen(process.env.PORT || 3000, () => {
-    console.log('app is running');
+  console.log('app is running');
 }); 
